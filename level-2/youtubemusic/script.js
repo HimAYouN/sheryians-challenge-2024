5f0a799a40967b2f17e59df8ae0401be42b25b69
HimAYouN <himayoun7931@gmail.com>
2024-02-16 11:57:24 +0530

    making music app

--- level-2/youtubemusic/script.js
+++ level-2/youtubemusic/script.js
@@ -4,77 +4,32 @@ var arr = [
     { songName: "Ram siya ram", url: "./songs/Ram Siya Ram.mp3", img: "./images/ram.jpg" },
     { songName: "Arjan Valley", url: "./songs/Arjan Vailly Ne.mp3", img: "./images/animal.jpg" }
 ]
-var allSongs = document.querySelector("#all-songs")
-var poster = document.querySelector("#left")
+var allSongs = document.querySelector("#all-songs");
+var audio = new Audio();
 
-var play = document.querySelector("#play")
-var backward = document.querySelector("#backward")
-var forward = document.querySelector("#forward")
-
-
-
-var audio = new Audio()
-
-var selectedSong = 0
-
-function mainFunction() {
-    var clutter = ""
-
-    arr.forEach(function (elem, index) {
-        clutter += `<div class="song-card" id=${index}>
-    <div class="part1">
-        <img src=${elem.img} alt="">
-        <h2>${elem.songName}</h2>
-    </div>
-    <h6>3:56</h6>
-</div>`
+function showSongs(){
+    var clutter = "";
+    arr.forEach((song, i)=>{
+        clutter+= `<div class="song-card" id="${i}">
+        <div class="part1">
+            <img src="${song.img}" alt="" />
+            <h2>${song.songName}</h2>
+        </div>
+        <h6>${i}</h6>
+    </div>`
     })
+    
     allSongs.innerHTML = clutter
-
-    audio.src = arr[selectedSong].url
-    poster.style.backgroundImage = `url(${arr[selectedSong].img})`
 }
-mainFunction()
 
-allSongs.addEventListener("click", function (dets) {
-    selectedSong = dets.target.id
-    mainFunction()
-    play.innerHTML = `<i class="ri-pause-mini-fill"></i>`
-    flag = 1
+allSongs.addEventListener('click', (e)=>{
+    console.log(arr[e.target.id].img)
+    audio.src = arr[e.target.id].url;
     audio.play()
+    document.querySelector('#left').style.backgroundImage = `url("${arr[e.target.id].img}")`
+
 })
 
-var flag = 0
 
-play.addEventListener("click", function () {
-    if (flag == 0) {
-        play.innerHTML = `<i class="ri-pause-mini-fill"></i>`
-        mainFunction()
-        audio.play()
-        flag = 1
-    } else {
-        play.innerHTML = `<i class="ri-play-mini-fill"></i>`
-        mainFunction()
-        audio.pause()
-        flag = 0
-    }
-})
 
-forward.addEventListener("click", function () {
-    if (selectedSong < arr.length - 1) {
-        selectedSong++
-        mainFunction()
-        audio.play()
-    }else{
-        forward.style.opacity = 0.4
-    }
-})
-backward.addEventListener("click", function () {
-    if (selectedSong > 0) {
-        selectedSong--
-        mainFunction()
-        audio.play()
-    }else{
-        backward.style.opacity = 0.4
-    }
-})
+showSongs();
